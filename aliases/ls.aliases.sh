64c306049bdc6ccbfd3e1f64026aa394c4ee878b
Albert.Xu <windxu@126.com>
2024-02-02 11:34:25 +0800

    update ls aliases configuration

--- aliases/ls.aliases.sh
+++ aliases/ls.aliases.sh
@@ -4,14 +4,14 @@
 # Directory Listing aliases
 alias dir='ls -hFx'
 alias l.='ls -d .* --color=tty' # short listing, only hidden files - .*
-alias l='ls -lathF'             # long, sort by newest to oldest
-alias L='ls -latrhF'            # long, sort by oldest to newest
+alias l='ls -lthF'             # long, sort by newest to oldest
+alias L='ls -ltrhF'            # long, sort by oldest to newest
 alias la='ls -Al'               # show hidden files
 alias lc='ls -lcr'              # sort by change time
 alias lk='ls -lSr'              # sort by size
 alias lh='ls -lSrh'             # sort by size human readable
 alias lm='ls -al | more'        # pipe through 'more'
-alias lo='ls -laSFh'            # sort by size largest to smallest
+alias lo='ls -lSFh'            # sort by size largest to smallest
 alias lr='ls -lR'               # recursive ls
 alias lt='ls -ltr'              # sort by date
 alias lu='ls -lur'              # sort by access time
